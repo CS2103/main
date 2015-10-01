74c1906b089b2bec02fdf563b437c81145c154b0
JamezMing <liuxiaoming8839@sina.com>
2015-10-01 15:20:51 +0800

    The command file updated

--- src/logic/Command.java
+++ src/logic/Command.java
@@ -2,41 +2,21 @@
 public class Command {
 	String COMMAND;
 	Task taskManipulation;
-	Events eventsManipulation;
-	Deadlines deadlineManipulation;
+
 	
 	public Command(String com, Task taskAltered){
 		COMMAND = com;
 		taskManipulation = taskAltered; 
 	}
-	
-	public Command(String com, Events eventsAltered){
-		COMMAND = com;
-		eventsManipulation = eventsAltered; 
-	}
-	
-	public Command(String com, Deadlines deadlinesAltered){
-		COMMAND = com;
-		deadlineManipulation = deadlinesAltered; 
-	}
+
 
 	public String returnCommand(){
 		return COMMAND;		
 	}
 	
-	
-	
-	public  returnMani(){
-		if(taskManipulation != null){
-			return taskManipulation;
-		}
-		else if()
-	}
-	
-	public Deadlines returnDeadlineMani(){
-		return deadlineManipulation;
-	}
-	public Events returnEventMani(){
-		return eventManipulation;
+	public Task returnMani(){
+		return taskManipulation;
 	}
 }
+	
+
