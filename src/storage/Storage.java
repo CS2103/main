13588a15943f74b78d31b19b12b653b3066cfc9b
hung123abc <nguyenthaihung07@gmail.com>
2015-10-04 03:07:59 +0800

    update Storage.java with specifying save location

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -16,26 +16,32 @@ import logic.Task;
 public class Storage {
 
 	private static Gson gson = new Gson();
-	private static File tempFile = new File("temp.json");
+	private static File tempFile = new File("tempFile.json");
+	private static File savedPath = new File("savedPath.txt");
 	private static String path;
+	private static BufferedReader br;
 
 	public Storage() {
 
 	}
 
 	public static void setPath(String path) {
-		File checkFile = new File(path);
-		if (checkFile.exists() && !checkFile.isDirectory()) {
-			System.out.println("File in used");
-		} else {
-			Storage.path = path;
+		try {
+			FileWriter fw = new FileWriter(savedPath.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(path);
+			bw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		Storage.path = path;
 	}
 
 	public static void write(ArrayList<Task> tasks){
 		try {
 			if (path == null) {
-				setPath(tempFile.getAbsolutePath());
+				setPath(savedPath.getAbsolutePath());
 			}
 			File file = new File(path);
 			FileWriter fw = new FileWriter(file);
@@ -52,10 +58,21 @@ public class Storage {
 	}
 
 	public static ArrayList<Task> read() {
+		try {
+			FileReader fr = new FileReader(savedPath);
+			br = new BufferedReader(fr);
+			Storage.path = br.readLine();
+		} 
+		catch (FileNotFoundException e) {
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		ArrayList<Task> taskList = new ArrayList<Task>();
 		String line = "";
-		if (path == null)
+		if (path == null) {
 			setPath(tempFile.getAbsolutePath());
+		}
 		try {
 			FileReader fr = new FileReader(path);
 			BufferedReader br = new BufferedReader(fr);
@@ -63,9 +80,9 @@ public class Storage {
 				taskList.add(gson.fromJson(line, Task.class));
 			}
 			br.close();
-		}
-		catch (FileNotFoundException e){
-		}
+		} 
+		catch (FileNotFoundException e) {
+		} 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
