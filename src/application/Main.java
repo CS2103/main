be1960b3b8f6ec65c9dbea15edee4482c05acb9c
HUNG <nguyenthaihung07@gmail.com>
2015-11-07 17:58:26 +0800

    update green theme

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 		GuiService myGui = new GuiService(stage);
 
 		myGui.showStage(); // Loads the GUI application window
-		HotKeyListener hkl = new HotKeyListener(stage);
+		//HotKeyListener hkl = new HotKeyListener(stage);
 		myGui.showTray();
 	}
 
